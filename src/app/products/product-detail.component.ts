d014f281b61ed9b434a61312094ed7f1f2badb2a
George Vigelette <gvigelet@duvitech.com>
2018-02-23 22:23:19 -0500

    protect routes

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,9 +10,28 @@ import { IProduct } from './product';
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
-  constructor() { }
+  constructor(private _route: ActivatedRoute, private _router: Router) {
+  }
 
   ngOnInit() {
+    const id = +this._route.snapshot.paramMap.get('id');
+    console.log('Selected Product ID: ' + id);
+
+    this.pageTitle += `: ${id}`;
+    this.product = {
+        'productId': id,
+        'productName': 'Garden Cart',
+        'productCode': 'GDN-0023',
+        'releaseDate': 'March 18, 2016',
+        'description': '15 gallon capacity rolling garden cart',
+        'price': 32.99,
+        'starRating': 4.2,
+        'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
+    };
+  }
+
+  onBack(): void {
+    this._router.navigate(['/products']);
   }
 
 }
